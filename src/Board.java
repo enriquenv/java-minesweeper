73e1f35c7a199c67e2c56300b05b9698ac7617fe
enriquenv <67122341+enriquenv@users.noreply.github.com>
2025-05-18 19:33:10 +1000

    displaying grid

--- src/Board.java
+++ src/Board.java
@@ -1,10 +1,39 @@
 public class Board {
 
+    private char[][] grid;
+    private int size = 10;
+
     public Board() {
-        System.out.println("Board created");
+        // System.out.println("Board created");
+        grid = new char[size][size];
+        initializeBoard();
+        System.out.println("Board initialized");
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = '?';
+            }
+        }
     }
 
     public void displayBoard() {
-        System.out.println("Board displayed");
+        // System.out.println("Board displayed");
+        System.out.println("\n  Current board:");
+        System.out.print("  ");
+        for (int j = 0; j < size; j++) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < size; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }

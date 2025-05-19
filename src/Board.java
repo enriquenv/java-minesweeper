ddd2ef8c23a4c733502224ccfb21a29e06047d25
enriquenv <67122341+enriquenv@users.noreply.github.com>
2025-05-19 20:04:37 +1000

    adding mines randomly

--- src/Board.java
+++ src/Board.java
@@ -1,21 +1,50 @@
+import java.util.Random;
+
 public class Board {
 
     private char[][] grid;
     private int size = 10;
+    private boolean[][] minesGrid;
+    private int numberOfMines = 10;
 
     public Board() {
-        // System.out.println("Board created");
         grid = new char[size][size];
+        minesGrid = new boolean[size][size];
         initializeBoard();
-        System.out.println("Board initialized");
+        placeMines();
+        System.out.println("Board initialized and mines hidden");
     }
 
     private void initializeBoard() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 grid[i][j] = '?';
+                minesGrid[i][j] = false;
             }
         }
+    }
+
+    private void placeMines() {
+        Random random = new Random();
+        int minesPlaced = 0;
+        while (minesPlaced < numberOfMines) {
+            int row = random.nextInt(size);
+            int col = random.nextInt(size);
+
+            if (!minesGrid[row][col]) {
+                minesGrid[row][col] = true;
+                minesPlaced++;
+            }
+        }
+        /*
+         * System.out.println("MinesGrid");
+         * for (int i = 0; i < size; i++) {
+         * for (int j = 0; j < size; j++) {
+         * System.out.print(minesGrid[i][j] ? "M " : ". ");
+         * }
+         * System.out.println();
+         * }
+         */
     }
 
     public void displayBoard() {
